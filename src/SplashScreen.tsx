914040cfbbe6fd3353dafe0890831ecdceb0a84d
Andreas Asséhn <andreas.assehn@gmail.com>
2022-09-22 12:47:49 +0200

    init

---
+++ src/SplashScreen.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+function SplashScreen() {
+  const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+  const [pokemons, setPokemons]: any[] = useState([]);
+
+  function getAllPokemons() {
+    const fetchedPokemons: any[] = [];
+    fetch(`${BASE_URL}/?limit=100`)
+      .then((res) => res.json())
+      .then((pokemons) =>
+        pokemons.results.forEach((pokemon: any) =>
+          getPokemonDetails(pokemon, fetchedPokemons)
+        )
+      );
+    setPokemons(fetchedPokemons);
+  }
+
+  function getPokemonDetails(pokemon: any, fetchedPokemons: any[]) {
+    return fetch(pokemon.url)
+      .then((res) => res.json())
+      .then((pokemon) => fetchedPokemons.push(pokemon));
+  }
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  return (
+    <>
+      <h1>SplashScreen</h1>
+    </>
+  );
+}
+
+export default SplashScreen;
