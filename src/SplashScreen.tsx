4ea78e6f6c57d8a733f68e277853a2c5fd1657d5
Andreas Asséhn <andreas.assehn@gmail.com>
2022-09-25 17:48:35 +0200

    adding page changer

--- src/SplashScreen.tsx
+++ src/SplashScreen.tsx
@@ -6,24 +6,34 @@ import { APIResource, Pokemon } from 'pokenode-ts';
 function SplashScreen() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [showAmount, setShowAmount] = useState(10);
+  const [page, setPage] = useState(0);
+  const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
   useEffect(() => {
     setPokemons([]);
-    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${showAmount}`)
+    fetch(`${BASE_URL}/?offset=${page * showAmount}}&limit=${showAmount}`)
       .then((res) => res.json())
       .then((data) =>
         data.results.forEach((pokemon: APIResource) =>
           fetch(pokemon.url)
             .then((res) => res.json())
-            .then((pokemon: Pokemon) =>
-              setPokemons((prevState: Pokemon[]) => [...prevState, pokemon])
+            .then((pokemon) =>
+              setPokemons((prevState) => [...prevState, pokemon])
             )
         )
       );
-  }, [showAmount]);
+  }, [showAmount, page]);
 
-  const handleAmount = (event: any) => {
+  const handleAmount = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setShowAmount(Number(event.target.value));
+  };
+
+  const prevPage = () => {
+    setPage((prevState) => prevState - 1);
+  };
+
+  const nextPage = () => {
+    setPage((prevState) => prevState + 1);
   };
 
   return (
@@ -31,12 +41,6 @@ function SplashScreen() {
       <header>
         <div className='header-content'>
           <select onChange={handleAmount}>
-            {/* <option
-              value='default'
-              disabled
-              selected
-              hidden
-            >{`Showing ${showAmount} results`}</option> */}
             <option value='10'>Show 10 results</option>
             <option value='20'>Show 20 results</option>
             <option value='50'>Show 50 results</option>
@@ -45,10 +49,43 @@ function SplashScreen() {
           <div>Sort dropdown</div>
         </div>
       </header>
+
+      <div className='change-page'>
+        {page < 1 ? (
+          <p className='button-disabled'>Previous page</p>
+        ) : (
+          <button onClick={prevPage}>Previous page</button>
+        )}
+        <p>
+          {page + 1} / {Math.ceil(1154 / showAmount)}
+        </p>
+        {page === Math.ceil(1154 / showAmount) ? (
+          <p className='button-disabled'>Previous page</p>
+        ) : (
+          <button onClick={nextPage}>Next page</button>
+        )}
+      </div>
+
       <div className='wrapper'>
         {pokemons.map((pokemon: Pokemon) => (
           <PokemonCard pokemon={pokemon} key={pokemon.id} />
         ))}
+      </div>
+
+      <div className='change-page'>
+        {page < 1 ? (
+          <p className='button-disabled'>Previous page</p>
+        ) : (
+          <button onClick={prevPage}>Previous page</button>
+        )}
+        <p>
+          {page + 1} / {Math.ceil(1154 / showAmount)}
+        </p>
+        {page === Math.ceil(1154 / showAmount) ? (
+          <p className='button-disabled'>Previous page</p>
+        ) : (
+          <button onClick={nextPage}>Next page</button>
+        )}
       </div>
     </div>
   );
